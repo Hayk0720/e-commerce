dbb1458ad0e38f8de5e21a818a88a21ba0a47f0f
Hayk Harutyunyan <haykprog0720@gmail.com>
2023-05-16 17:39:19 +0400

    classComp

---
+++ src/components/Item.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Item extends Component {
+  render() {
+    return (
+      
+        <div className='item' >
+            <img width={200} src='./../../img/chair.jpeg' alt={this.props.item.title}/>
+            <h2>{this.props.item.title}</h2>
+            <p>{this.props.item.desc}</p>
+            <b>{`${this.props.item.price}$`}</b>
+            <div className='add-to-cart'>+</div>
+        </div>
+     
+    )
+  }
+}
+
+export default Item
